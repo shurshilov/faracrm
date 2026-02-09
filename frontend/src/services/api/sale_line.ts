fe7933c9105542e522f37370c8684b25b95a8bfe
Artem Shurshilov <shurshilov.a@yandex.ru>
2026-02-09 11:48:31 +0300

    [REF] удалены не исопльзуемые роуты

--- frontend/src/services/api/sale_line.ts
+++
@@ -1,7 +0,0 @@
-import { crudApi as api } from './crudApi';
-const injectedRtkApi = api.injectEndpoints({
-  endpoints: build => ({}),
-  overrideExisting: false,
-});
-export { injectedRtkApi as crudApi };
-export const {} = injectedRtkApi;
